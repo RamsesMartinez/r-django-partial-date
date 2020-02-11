525679efee6638d23d4f3ae31702e78ef5e367c2
Ramses Martinez <ramses.mtz96@gmail.com>
2020-02-10 22:16:43 -0600

    Refactor docs

--- setup.py
+++ setup.py
@@ -1,7 +1,30 @@
+import sys
+
 from setuptools import setup
 
 from codecs import open
 from os import path
+
+CURRENT_PYTHON = sys.version_info[:2]
+REQUIRED_PYTHON = (3, 6)
+
+# This check and everything above must remain compatible with Python 2.7.
+if CURRENT_PYTHON < REQUIRED_PYTHON:
+    sys.stderr.write("""
+==========================
+Unsupported Python version
+==========================
+This version of r-django-partial-date requires Python {}.{}, but you're trying to
+install it on Python {}.{}.
+This may be because you are using a version of pip that doesn't
+understand the python_requires classifier. Make sure you
+have pip >= 9.0 and setuptools >= 24.2, then try again:
+    $ python -m pip install --upgrade pip setuptools
+    $ python -m pip install django
+This will install the latest version of r-django-partial-date which works on your
+version of Python.
+""".format(*(REQUIRED_PYTHON + CURRENT_PYTHON)))
+    sys.exit(1)
 
 here = path.abspath(path.dirname(__file__))
 
@@ -9,24 +32,21 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='django_partial_date',
-    version='1.2.2',
-
+    name='r-django-partial-date',
+    version='1.3.0',
     description='Django custom model field for partial dates with the form YYYY, YYYY-MM, YYYY-MM-DD',
     long_description=long_description,
-
-    url='https://github.com/ktowen/django_partial_date',
-
-    author='ktowen',
-    author_email='towenpa@gmail.com',
-
+    url='https://github.com/RamsesMartinez/django_partial_date',
+    author='Ramses Martinez',
+    author_email='ramses.mtz96@gmail.com',
     license='MIT',
-
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     keywords=['fields', 'django', 'dates', 'partial'],
 
